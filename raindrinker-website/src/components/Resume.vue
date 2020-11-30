df26735c9d4cb9006583429e16b452423b8500f3
Raindrinker <iamtheraindrinker@gmail.com>
2020-11-30 23:01:14 +0100

    Resume

--- raindrinker-website/src/components/Resume.vue
+++ raindrinker-website/src/components/Resume.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-        SOON
+    <div class="body">
+        <ResumeDocument class="doc"/>
     </div>
 </template>
 
 <script>
+    import ResumeDocument from "./ResumeDocument";
     export default {
-        name: "Resume"
+        name: "Resume",
+        components: {ResumeDocument}
     }
 </script>
 
 <style scoped>
+    .body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
+        padding: 64px 32px;
+
+        overflow-y: auto;
+    }
+
+    .doc {
+        -webkit-box-shadow: 0px 5px 2px 0px rgba(0, 0, 0, 0.25);
+        -moz-box-shadow: 0px 5px 2px 0px rgba(0, 0, 0, 0.25);
+        box-shadow: 0px 5px 2px 0px rgba(0, 0, 0, 0.25);
+    }
 </style>
---
+++ raindrinker-website/src/components/ResumeDocument.vue
@@ -0,0 +1,266 @@
+<template>
+    <div class="document">
+        <div class="title1">
+            Ferran Ruiz Sala
+        </div>
+        <div class="job">
+            Game Developer
+        </div>
+        <div class="location">
+            Barcelona, Spain
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">person</div>
+            Profile
+        </div>
+
+        <div class="leftpad">
+
+            <div class="text">
+                Computer engineer and indie gamedev. <br>
+                <ul>
+                    <li>Experience in most engines: UE4, Unity, GameMaker & Godot.</li>
+                    <li>Successful hobbyist game jam developer.</li>
+                    <li>Passionate about discussing games and game design.</li>
+                    <li>Active online in multiple indie development communities, mostly on Twitter and Discord.</li>
+                </ul>
+                Aware of most of the indie gaming cosmos, knowledgeable about trends and indie games and the discussions that are happening in the industry both about game design and other issues.
+                Always glad to work on diverse and engaged creative teams and share and receive feedback.
+            </div>
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">games</div>
+            Gamedev experience
+        </div>
+
+        <div class="leftpad">
+
+            <div class="title3">
+                Game Designer at Abylight
+            </div>
+            <div class="date">
+                2020-...
+            </div>
+
+            <div class="leftpad">
+                Game Design of big tycoon games including:
+                <ul>
+                    <li>Designing mechanics and systems
+                    </li>
+                    <li>Prototyping
+                    </li>
+                    <li>Producing and managing content for the games, like text, characters and items
+                    </li>
+                </ul>
+            </div>
+
+            <div class="title3">
+                Participant in lots of game jams
+            </div>
+            <div class="date">
+                2017-...
+            </div>
+
+            <div class="leftpad">
+                Participant and winner of lots of game jams, both in-person and online, solo and in a team.
+                <ul>
+                    <li>12th place in Ludum dare 41
+                    </li>
+                    <li>Itch.io profile with >4.200 downloads and 28.000 views
+                    </li>
+                    <li>Active member in lots of gamedev and game jam internet communities
+                    </li>
+                </ul>
+            </div>
+
+            <div class="title3">
+                Gamedev NPO member
+            </div>
+            <div class="date">
+                2017-2019
+            </div>
+
+            <div class="leftpad">
+                Member of the council of volunteers that manage the Alakajam website, a non-profit gamedev community that organises regular jams.
+            </div>
+
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">image</div>
+            Portfolio
+        </div>
+
+        <div class="leftpad">
+            <p>Website: raindrinker.com
+            </p>
+            <p>itch.io: https://raindrinker.itch.io/
+            </p>
+            <p>Github: https://github.com/Raindrinker
+            </p>
+            <p>Twitter: https://twitter.com/Raindrinker_dev
+            </p>
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">school</div>
+            Education
+        </div>
+
+        <div class="leftpad">
+
+            <div class="title3">
+                Master in Advanced Videogame Development, La Salle Campus Barcelona
+            </div>
+            <div class="date">
+                2018-2019
+            </div>
+
+            <div class="leftpad">
+                Master in La Salle which covers technical skills for developing gamedev engines, working in UE4 and Unity, and other aspects of professional videogame development.
+            </div>
+
+            <div class="title3">
+                Degree in Computer Engineering + Multimedia Engineering, La Salle Campus Barcelona
+            </div>
+            <div class="date">
+                2013-2018
+            </div>
+
+            <div class="leftpad">
+                Double degree in La Salle which allows students to finish university studies in 5 years with two official Bachelor's Degrees. This double degree merges the creative and design side with deep knowledge about programming and computer architecture.
+                Best academic expedient for both courses.
+            </div>
+
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">work</div>
+            Other work
+        </div>
+
+        <div class="leftpad">
+
+            <div class="title3">
+                Junior Frontend Sofware Developer at Verbio, Barcelona
+            </div>
+            <div class="date">
+                2018-2020
+            </div>
+
+            <div class="leftpad">
+                Working in a team specialized in Natural Language Understanding and bot development
+                <ul>
+                    <li>
+                        Designing and developing web tools and dashboards to manage NLU applications
+                    </li>
+                    <li>
+                        Frontend programming and design in Vue
+                    </li>
+                    <li>
+                        Agile software development
+                    </li>
+                </ul>
+            </div>
+
+            <div class="title3">
+                Intern Associate at La Salle Campus Barcelona
+            </div>
+            <div class="date">
+                2015-2018
+            </div>
+
+            <div class="leftpad">
+                Working as an associate teacher alongside the teachers to support in two college courses:
+                <ul>
+                    <li>
+                        Introduction to programming
+                    </li>
+                    <li>
+                        Digital systems and microprocessors
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">chat_bubble</div>
+            Contact
+        </div>
+
+        <div class="leftpad">
+            <p>Discord: Raindrinker#2532
+            </p>
+            <p>Email: iamtheraindrinker@gmail.com
+            </p>
+            <p>Phone: 636039184
+            </p>
+        </div>
+
+        <div class="title2-row">
+            <div class="icon">language</div>
+            Languages
+        </div>
+
+        <div class="leftpad">
+            <p>English: Very good
+            </p>
+            <p>Spanish: Native
+            </p>
+            <p>Catalan: Native
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ResumeDocument"
+    }
+</script>
+
+<style scoped>
+    .document {
+        background-color: white;
+        max-width: 80%;
+        width: 100%;
+        padding: 32px;
+        font-family: Nunito, sans-serif;
+    }
+
+    .title1 {
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .title2-row {
+        display: flex;
+        font-size: 24px;
+        margin-top: 32px;
+        margin-bottom: 8px;
+    }
+
+    .icon {
+        font-family: 'Material Icons';
+        font-size: 32px;
+        margin-right: 8px;
+    }
+
+    .title3 {
+        font-weight: bold;
+        margin-top: 16px;
+    }
+
+    .date {
+        color: #666666;
+        margin-bottom: 16px;
+    }
+
+    .leftpad {
+        padding-left: 16px;
+    }
+</style>
